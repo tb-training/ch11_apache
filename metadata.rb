c141fc2c179619eedd91de72fd4c9571ffafe40d
dev1 <tio.bagio@gmail.com>
2019-08-26 12:10:13 +0800

    change version

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,17 @@ maintainer_email 'tbagio@chef.io.com'
 license 'All Rights Reserved'
 description 'Installs/Configures ch4_webserver'
 long_description 'Installs/Configures ch4_webserver'
-version '0.2.0'
+version '0.3.1'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/ch4_webserver/issues'
+# issues_url 'https://github.com/<insert_org_here>/ch11_apache/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/ch4_webserver'
+# source_url 'https://github.com/<insert_org_here>/ch11_apache'
